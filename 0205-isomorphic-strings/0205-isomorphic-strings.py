abdc595df9d9e5e6dccee78942a3f9dd99bd0c52
Kanak Shrivastava <144111539+KanakShrivastava@users.noreply.github.com>
2024-08-05 18:18:17 +0530

    Time: 49 ms (20.98%), Space: 16.7 MB (34.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -3,22 +3,12 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        s_to_t = {}
-        t_to_s = {}
+        mapST, mapTS = {}, {}
+        for i in range(len(s)):
+            c1, c2 = s[i], t[i]
+            if((c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1)):
+                return False
 
-        for char_s, char_t in zip(s, t):
-            if char_s in s_to_t:
-                if s_to_t[char_s] != char_t:
-                    return False
-
-            else:
-                s_to_t[char_s] = char_t
-
-            if char_t in t_to_s:
-                if t_to_s[char_t] != char_s:
-                    return False
-
-            else:
-                t_to_s[char_t] = char_s
-
+            mapST[c1] = c2
+            mapTS[c2] = c1
         return True
